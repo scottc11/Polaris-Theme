d49b14eaf08f388bd8b53e92604d277a2d954bf7
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2016-09-28 10:35:44 -0400

    created a basic counter for products loop.  Does nothing so far.

--- loop-products.php
+++ loop-products.php
@@ -3,6 +3,22 @@
 <!-- START OF LOOP --> <!-- query arguments set in products page -->
 <?php if ( $productsQuery->have_posts() ) : while ( $productsQuery->have_posts() ) : $productsQuery->the_post(); ?>
 
+<?php
+  $counter = 1;
+
+  if ($counter == 1) {
+    $counter += 1;
+  }
+
+  if ($counter == 2) {
+    $counter += 1;
+  }
+
+  if ($counter == 3) {
+    $counter = 1; // reset counter to 1
+  }
+?>
+
 
 <div class="col-xs-6 col-md-4">
   <div class="product-summary-container">
