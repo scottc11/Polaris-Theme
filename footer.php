2f779b7a3b051fdd857fad41008eeda1cd4aa22f
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2016-11-17 14:54:51 -0500

    removed analytics script in footer.

--- footer.php
+++ footer.php
@@ -38,15 +38,6 @@
 
 		<?php wp_footer(); ?>
 
-		<!-- analytics -->
-		<script>
-			(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-			(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-			l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-			ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-			ga('send', 'pageview');
-		</script>
 
 		<!-- JQUERY -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
